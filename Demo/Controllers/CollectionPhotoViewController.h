9eecc5a53ababb34fe851f38cb1c5c4b51379094
Fábio Bernardo <mantyftw@gmail.com>
2013-10-09 00:11:06 +0100

    v3

---
+++ Demo/Controllers/CollectionPhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionPhotoViewController.h
+//  Demo
+//
+//  Created by Rodrigo on 08/10/13.
+//  Copyright (c) 2013 fctw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionPhotoViewController : UICollectionViewController
+
+@end
---
+++ Demo/Controllers/CollectionPhotoViewController.m
@@ -0,0 +1,38 @@
+//
+//  CollectionPhotoViewController.m
+//  Demo
+//
+//  Created by Rodrigo on 08/10/13.
+//  Copyright (c) 2013 fctw. All rights reserved.
+//
+
+#import "CollectionPhotoViewController.h"
+
+@interface CollectionPhotoViewController ()
+
+@end
+
+@implementation CollectionPhotoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
