da428ff1ce530c157f36f6610c37a229ede43875
Fábio Bernardo <mantyftw@gmail.com>
2013-10-08 23:25:26 +0100

    v0

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Rodrigo on 08/10/13.
+//  Copyright (c) 2013 fctw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by Rodrigo on 08/10/13.
+//  Copyright (c) 2013 fctw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
